f9fa7aee578915e8c44fc6cfc4e0e5d1005a5424
periabdraimova03 <peri.abdraimova@gmail.com>
2026-01-29 22:37:33 +0000

    hw done

--- homework-1/main.tf
+++ homework-1/main.tf
@@ -12,20 +12,45 @@ resource "aws_iam_user" "user3" {
 resource "aws_iam_user" "user4" {
   name = "jisoo"
 }
-resource "aws_iam_group" "group1" {
+resource "aws_iam_user" "user5" {
+  name = "jihyo"
+}
+resource "aws_iam_user" "user6" {
+  name = "sana"
+}
+resource "aws_iam_user" "user7" {
+  name = "momo"
+}
+resource "aws_iam_user" "user8" {
+  name = "dahyun"
+}  
+resource "aws_iam_group" "blackpink" {
   name = "blackpink"
 }
-resource "aws_iam_group" "group2" {
+resource "aws_iam_group" "twice" {
   name = "twice"
 }
-resource "aws_iam_group_membership" "team" {
-  name = "tf-testing-group-membership"
+resource "aws_iam_group_membership" "blackpink_members" {
+  name = "blackpink-membership"
 
   users = [
-    aws_iam_user.hello1.name,
-    aws_iam_user.hello2.name,
-    aws_iam_user.hello3.name,
+    aws_iam_user.user1.name,
+    aws_iam_user.user2.name,
+    aws_iam_user.user3.name,
+    aws_iam_user.user4.name,
   ]
 
-  group = aws_iam_group.group1.name
+  group = aws_iam_group.blackpink.name
+}
+resource "aws_iam_group_membership" "twice_members" {
+  name = "twice-membership"
+
+  users = [
+    aws_iam_user.user5.name,
+    aws_iam_user.user6.name,
+    aws_iam_user.user7.name,
+    aws_iam_user.user8.name,
+  ]
+
+  group = aws_iam_group.twice.name
 }
