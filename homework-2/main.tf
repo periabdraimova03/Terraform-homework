1ebb34be55186bcd1116358fdd3ce9b031e3bf5c
periabdraimova03 <peri.abdraimova@gmail.com>
2026-02-04 23:20:58 +0000

    hw2 added

---
+++ homework-2/main.tf
@@ -0,0 +1,32 @@
+resource "aws_iam_user" "user1" {
+  name = "jenny"
+}
+
+resource "aws_iam_user" "user2" {
+    depends_on = [aws_iam_user.user1]
+  name = "rose"
+}
+
+resource "aws_iam_user" "user3" {
+    depends_on = [aws_iam_user.user2]
+  name = "lisa"
+}
+resource "aws_iam_user" "user4" {
+    depends_on = [aws_iam_user.user3]
+  name = "jisoo"
+}
+resource "aws_iam_user" "user5" {
+    depends_on = [aws_iam_user.user4]
+  name = "jihyo"
+}
+resource "aws_iam_user" "user6" {
+    depends_on = [aws_iam_user.user5]
+  name = "sana"
+}
+resource "aws_iam_user" "miyeon" {
+  name = "miyeon"
+}
+resource "aws_iam_user" "mina" {
+  name = "mina"
+}
+
